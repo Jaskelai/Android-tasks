45c804c543a913ef72766e18e59f32ba241010ed
KornilovMikhail <31703085+KornilovMikhail@users.noreply.github.com>
2018-09-30 22:04:09 +0300

    homework completed

---
+++ app/src/main/java/com/github/kornilovmikhail/homework/EditDialog.java
@@ -0,0 +1,57 @@
+package com.github.kornilovmikhail.homework;
+
+import android.app.Dialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.DialogFragment;
+import android.support.v7.app.AlertDialog;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.EditText;
+
+public class EditDialog extends DialogFragment {
+
+    private EditText etLogin;
+    private EditText etEmail;
+    private CallBack callBack;
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        callBack = (CallBack) context;
+    }
+
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
+        super.onCreateDialog(savedInstanceState);
+        View view = LayoutInflater.from(getActivity()).inflate(R.layout.dialog_edit, null);
+        etLogin = view.findViewById(R.id.et_login);
+        etEmail = view.findViewById(R.id.et_email);
+        String title = "Set login and email";
+        AlertDialog.Builder ad = new AlertDialog.Builder(getActivity());
+        ad.setTitle(title);
+        ad.setView(view);
+        ad.setPositiveButton("Save", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                callBack.callback(etLogin.getText().toString(), etEmail.getText().toString());
+            }
+        });
+        ad.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+            }
+        });
+        return ad.show();
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        callBack = null;
+    }
+}
--- app/src/main/java/com/github/kornilovmikhail/homework/MainActivity.java
+++ app/src/main/java/com/github/kornilovmikhail/homework/MainActivity.java
@@ -1,88 +1,91 @@
 package com.github.kornilovmikhail.homework;
 
-import android.content.Intent;
-import android.support.annotation.Nullable;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.widget.TextView;
-import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity implements CallBack {
 
-    private final int PICK_INFO_REQUEST = 1;
-    private final int PICK_RESULT_SENDING_REQUEST = 0;
-    private TextView textName;
-    private TextView textEmail;
-    private TextView textPhone;
+    private DrawerLayout drawerLayout;
+    private NavigationView navigationView;
+    private InfoFragment infoFragment;
+    private MessageFragment messageFragment;
+    private ProfileFragment profileFragment;
+    private TextView navLogin;
+    private TextView navEmail;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        textName = findViewById(R.id.tv_main_name);
-        textEmail = findViewById(R.id.tv_main_email);
-        textPhone = findViewById(R.id.tv_main_phone);
-        Button editButton = findViewById(R.id.btn_main_edit);
-        editButton.setOnClickListener(this);
-        Button sendButton = findViewById(R.id.btn_main_send_name);
-        sendButton.setOnClickListener(this);
+        infoFragment = new InfoFragment();
+        messageFragment = new MessageFragment();
+        profileFragment = new ProfileFragment();
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        ActionBar actionbar = getSupportActionBar();
+        actionbar.setDisplayHomeAsUpEnabled(true);
+        actionbar.setHomeAsUpIndicator(R.drawable.ic_menu);
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.nav_view);
+        selectDrawerItem(navigationView);
+        transaction(infoFragment);
     }
 
     @Override
-    public void onClick(View view) {
-        switch (view.getId()) {
-            case R.id.btn_main_edit:
-                Intent intentEdit = new Intent(MainActivity.this, EditActivity.class);
-                if (textName.getText() != null && textEmail.getText() != null &&
-                        textPhone.getText() != null) {
-                    intentEdit.putExtra(EditActivity.NAME, textName.getText());
-                    intentEdit.putExtra(EditActivity.EMAIL, textEmail.getText());
-                    intentEdit.putExtra(EditActivity.PHONE, textPhone.getText());
-                }
-                startActivityForResult(intentEdit, PICK_INFO_REQUEST);
-                break;
-            case R.id.btn_main_send_name:
-                if (!textName.getText().equals("") && !textEmail.getText().equals("")) {
-                    Intent intentSend = new Intent(Intent.ACTION_SEND);
-                    intentSend.setType("text/plain");
-                    intentSend.putExtra(Intent.EXTRA_TEXT, textName.getText().toString());
-                    startActivityForResult(intentSend, PICK_RESULT_SENDING_REQUEST);
-                } else {
-                    String warning = "Name is empty";
-                    Toast toast = Toast.makeText(getApplicationContext(), warning, Toast.LENGTH_SHORT);
-                    toast.show();
-                }
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                drawerLayout.openDrawer(GravityCompat.START);
+                return true;
         }
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void selectDrawerItem(NavigationView nv) {
+        nv.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                switch (menuItem.getItemId()) {
+                    case R.id.nav_info:
+                        transaction(infoFragment);
+                        break;
+                    case R.id.nav_message:
+                        transaction(messageFragment);
+                        break;
+                    case R.id.nav_profile:
+                        transaction(profileFragment);
+                        break;
+                    default:
+                        return true;
+                }
+                drawerLayout.closeDrawer(GravityCompat.START);
+                return true;
+            }
+        });
+    }
+
+    public void transaction(Fragment fragment) {
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.content_frame, fragment);
+        transaction.commit();
     }
 
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        if (requestCode == PICK_INFO_REQUEST) {
-            if (resultCode == RESULT_OK) {
-                String nameFromEdit = data.getStringExtra(EditActivity.NAME);
-                String emailFromEdit = data.getStringExtra(EditActivity.EMAIL);
-                String phoneFromEdit = data.getStringExtra(EditActivity.PHONE);
-                textName.setText(nameFromEdit);
-                textEmail.setText(emailFromEdit);
-                textPhone.setText(phoneFromEdit);
-            }
-            if (resultCode == RESULT_CANCELED) {
-                textName.setText("");
-                textEmail.setText("");
-                textPhone.setText("");
-                String message = "Canceled";
-                Toast.makeText(this,message,Toast.LENGTH_SHORT)
-                        .show();
-            }
-        }
-        if (requestCode == PICK_RESULT_SENDING_REQUEST) {
-            if (resultCode == RESULT_OK) {
-                String message = "Name sent";
-                Toast.makeText(this, message, Toast.LENGTH_SHORT)
-                        .show();
-            }
-        }
+    public void callback(String login, String email) {
+        profileFragment.save(login, email);
+        navLogin = findViewById(R.id.nav_login);
+        navEmail = findViewById(R.id.nav_email);
+        navLogin.setText(login);
+        navEmail.setText(email);
     }
 }
