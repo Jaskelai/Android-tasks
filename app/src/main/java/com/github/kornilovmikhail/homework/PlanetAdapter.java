7d8bf029b5ecb994afe1623ee1e83fa9ac82da7e
KornilovMikhail <31703085+KornilovMikhail@users.noreply.github.com>
2018-10-08 01:00:17 +0300

    add image to list

--- app/src/main/java/com/github/kornilovmikhail/homework/PlanetAdapter.java
+++ app/src/main/java/com/github/kornilovmikhail/homework/PlanetAdapter.java
@@ -1,10 +1,12 @@
 package com.github.kornilovmikhail.homework;
 
+import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class PlanetAdapter extends RecyclerView.Adapter<PlanetAdapter.PlanetHolder> {
@@ -12,22 +14,28 @@ public class PlanetAdapter extends RecyclerView.Adapter<PlanetAdapter.PlanetHold
             "solar system", R.drawable.ic_planet_mercury), new Planet("Venus", "Second" +
             "planet in solar system", R.drawable.ic_planet_venus), new Planet("Earth",
             "Third planet in solar system", R.drawable.ic_planet_earth), new Planet("Mars",
-            "Fourth planet in solar system", R.drawable.ic_planet_jupiter), new Planet("Jupiter",
+            "Fourth planet in solar system", R.drawable.ic_planet_mars), new Planet("Jupiter",
             "Fifth planet in solar system", R.drawable.ic_planet_jupiter)};
 
     private Callback callback;
 
     public class PlanetHolder extends RecyclerView.ViewHolder {
-        TextView tvPlanet;
+        View item;
+        ImageView imageView;
+        TextView textView;
 
-        public PlanetHolder(TextView v) {
+        public PlanetHolder(View v, Context context) {
             super(v);
-            tvPlanet = v;
+            item = v;
+            callback = (Callback)context;
         }
 
-        public void bind(String name) {
-            tvPlanet.setText(name);
-            tvPlanet.setOnClickListener(new View.OnClickListener() {
+        public void bind(String name, int image) {
+            imageView = item.findViewById(R.id.list_image);
+            imageView.setImageResource(image);
+            textView = item.findViewById(R.id.list_text);
+            textView.setText(name);
+            item.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
                     int position = getAdapterPosition();
@@ -41,14 +49,13 @@ public class PlanetAdapter extends RecyclerView.Adapter<PlanetAdapter.PlanetHold
 
     @Override
     public PlanetAdapter.PlanetHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        callback = (Callback) parent.getContext();
-        TextView v = (TextView) LayoutInflater.from(parent.getContext()).inflate(R.layout.list_item, parent, false);
-        return new PlanetHolder(v);
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.list_item, parent, false);
+        return new PlanetHolder(v,parent.getContext());
     }
 
     @Override
     public void onBindViewHolder(@NonNull PlanetHolder planetHolder, int i) {
-        planetHolder.bind(planets[i].getName());
+        planetHolder.bind(planets[i].getName(),planets[i].getImage());
     }
 
     @Override
